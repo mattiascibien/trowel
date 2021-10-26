0867f4b44b6ce30028ed6265b21416b9db823a3c
Mattias Cibien <mattias.cibien@outlook.com>
2021-10-26 16:34:14 +0200

    Parsing of entities file

--- Trowel.Providers/GameData/Ent/EntProvider.cs
+++ Trowel.Providers/GameData/Ent/EntProvider.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using System.Text;
 using System.Xml.Linq;
+using Trowel.DataStructures.GameData;
 
 namespace Trowel.Providers.GameData
 {
@@ -15,30 +18,109 @@ namespace Trowel.Providers.GameData
 
             foreach (var element in entElement.Elements())
             {
-                if(element.Name == "point")
-                    gameData.Classes.Add(ParsePointEntity(element));
+                if (element.Name == "point")
+                    gameData.Classes.Add(ParseEntity(element, DataStructures.GameData.ClassType.Point));
                 else if (element.Name == "group")
-                    gameData.Classes.Add(ParseBrushEntity(element));
+                    gameData.Classes.Add(ParseEntity(element, DataStructures.GameData.ClassType.Solid));
                 else
                     throw new ProviderException($"Tag {element.Name} not recognized");
             }
 
-
             return gameData;
         }
 
-        private DataStructures.GameData.GameDataObject ParseBrushEntity(XElement element)
+        private DataStructures.GameData.GameDataObject ParseEntity(XElement element, DataStructures.GameData.ClassType classType)
         {
-            var gdo = new DataStructures.GameData.GameDataObject(element.Attribute("name").Value, "", DataStructures.GameData.ClassType.Solid);
+            var gdo = new DataStructures.GameData.GameDataObject(element.Attribute("name").Value, BuildEntityDescription(element.Value), classType);
+
+            foreach (var attribute in element.Attributes().Where(a => a.Name != "name"))
+            {
+                Behaviour behaviour;
+                switch (attribute.Name.LocalName)
+                {
+                    case "color":
+                        behaviour = new Behaviour(attribute.Name.LocalName, 
+                            string.Join(" ", attribute.Value.Split(" ")
+                                .Select(float.Parse)
+                                .Select(f => (int)MathF.Round(f * 255))
+                                .Select(i => i.ToString())
+                                ));
+                        break;
+                    case "box":
+                        string[] values = attribute.Value.Split();
+
+                        string first = string.Join(" ", values.Take(3).ToArray());
+                        string second = string.Join(" ", values.Skip(3).Take(3).ToArray());
+
+                        behaviour = new Behaviour("size", first, second);
+                        break;
+                    default:
+                        throw new ProviderException($"Unknown behavior {attribute.Name.LocalName}");
+                }
+                gdo.Behaviours.Add(behaviour);
+            }
+
+            foreach (var item in element.Elements())
+            {
+                VariableType variableType = StringToVariableType(item.Name.LocalName);
+                Property prop = new Property(item.Attribute("key").Value, variableType)
+                {
+                    Description = item.Value,
+                };
+
+                gdo.Properties.Add(prop);
+            }
 
             return gdo;
         }
 
-        private DataStructures.GameData.GameDataObject ParsePointEntity(XElement element)
+        private static string BuildEntityDescription(string value)
         {
-            var gdo = new DataStructures.GameData.GameDataObject(element.Attribute("name").Value, "", DataStructures.GameData.ClassType.Point);
+            StringBuilder builder = new StringBuilder();
 
-            return gdo;
+            using (StringReader reader = new StringReader(value))
+            {
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    if (!line.StartsWith("--------"))
+                        builder.AppendLine(line);
+
+                    line = reader.ReadLine();
+                }    
+            }
+
+            return builder.ToString();
+        }
+
+        private static VariableType StringToVariableType(string name)
+        {
+            switch (name)
+            {
+                case "direction":
+                    return VariableType.String;
+                case "angles":
+                    return VariableType.Angle;
+                case "target":
+                    return VariableType.TargetDestination;
+                case "targetname":
+                    return VariableType.TargetSource;
+                case "string":
+                    return VariableType.String;
+                case "real":
+                    return VariableType.Float;
+                case "real3":
+                    return VariableType.String;
+                case "sound":
+                    return VariableType.Sound;
+                case "model":
+                    return VariableType.Studio;
+                // TODO: other cases?
+                case "flag":
+                    return VariableType.Flags;
+                default:
+                    return VariableType.String;
+            }
         }
     }
 }
